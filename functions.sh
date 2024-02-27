c60d9bcd001b15a6cd8f5768125be6e4c279c94b
David Carlos Manuelda <StormByte@gmail.com>
2024-02-27 21:00:50 +0100

    Updated bundled functions.sh

--- functions.sh
+++ functions.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-# Version 1.0.1
+# Version 1.4.1
 
 function displayError() {
 	echo $1
@@ -44,6 +44,59 @@ function loadConfig() {
 		echo "Configuration file ${self}.conf not found neither in current directory neither in /etc/conf.d!"
 		exit 1
 	fi
+}
+
+function list_contains() { [[ "$1" =~ (^|[[:space:]])"$2"($|[[:space:]]) ]]; }
+
+function force_binutils_vars() {
+    ADDR2LINE="addr2line"
+    AS="as"
+    AR="ar"
+    NM="nm"
+    OBJCOPY="objcopy"
+    OBJDUMP="objdump"
+    RANLIB="ranlib"
+    READELF="readelf"
+    STRINGS="strings"
+    STRIP="strip"
+}
+
+function force_gcc_vars() {
+    OCC="gcc"
+    OCXX="g++"
+	CC="gcc"
+	CXX="g++"
+	CPP="cpp"
+    CFLAGS="${COMPILER_OPTIMIZATION_BASE} ${COMPILER_OPTIMIZATION_CPU} ${COMPILER_OPTIMIZATION_GCC} ${COMPILER_OPTIMIZATION_CET} -fno-builtin-strlen"
+    CXXFLAGS="${CFLAGS}"
+    LDFLAGS="${LINKER_OPTIMIZATION_BASE} ${LINKER_OPTIMIZATION_BFD}"
+    force_binutils_vars
+}
+
+function force_cxx11_vars() {
+    CXXFLAGS="${CXXFLAGS} -std=c++11"
+}
+
+function force_pic_vars() {
+    CFLAGS="${CFLAGS} -fPIC"
+    CXXFLAGS="${CXXFLAGS} -fPIC"
+}
+
+function force_lto_vars() {
+    CFLAGS="${CFLAGS} ${COMPILER_OPTIMIZATION_LTO}"
+    CXXFLAGS="${CXXFLAGS} ${COMPILER_OPTIMIZATION_LTO}"
+    LDFLAGS="${LDFLAGS} ${COMPILER_OPTIMIZATION_LTO}"
+    RUSTFLAGS="${RUSTFLAGS} -Clinker-plugin-lto"
+}
+
+function force_polly_disable {
+	CFLAGS="${COMPILER_OPTIMIZATION_BASE} ${COMPILER_OPTIMIZATION_CPU} -Wno-unused-command-line-argument ${COMPILER_OPTIMIZATION_CET}"
+	CXXFLAGS="${COMPILER_OPTIMIZATION_BASE} ${COMPILER_OPTIMIZATION_CPU} -Wno-unused-command-line-argument ${COMPILER_OPTIMIZATION_CET}"
+	LDFLAGS="${LINKER_OPTIMIZATION_BASE}"
+}
+
+function force_ld_undefined_version {
+	LDFLAGS="${LINKER_OPTIMIZATION_BASE} -Wl,--undefined-version" 
 }
 
 # Useful variables
